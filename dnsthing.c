cb1e9a688c5e164efe196a6f39b7f64ba376c0a1
kieroid <40911271+kieroid@users.noreply.github.com>
2024-09-14 11:52:44 -0500

    stuff

--- dnsthing.c
+++ dnsthing.c
@@ -34,13 +34,33 @@ void die(const char *fmt, ...)
         exit(1);
 }
 
+/* primary funcs */
+void startDaemon() {
+
+}
+
+void showHelp() {
+	die(
+			"Usage: dnsthing [options]\n"
+			"Example: dnsthing daemon start\n\n"
+			"  uninstall	- uninstall dnsthing from the current machine\n"
+			"  status	- check the current status of the daemon\n"
+			"  adlist	- manage currently used adlists\n"
+			"  domain	- manage currently blocked domains (seperate from the adlist)\n"
+			"  exceptions	- manage exceptions to domains posed from the adlist\n"
+			"  daemon	- manage the current stateus of the daemon\n"
+			);
+}
+
 int main(int argc, char *argv[])
 {
 	/* extra arguments/settings */
 	if (argc >= 2 && (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0))
 		die("dnsthing-%s", VER);
 	else if (argc >= 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0))
-		die("yousmell");
+		showHelp();
+	else if (argc >= 2 && strcmp(argv[1], "daemon") == 0)
+		startDaemon();	
 	else if (argc != 1)
 		die("usage");
 }
