82f576f97c855b954c7698721f638a2251850aca
kieroid <40911271+kieroid@users.noreply.github.com>
2024-09-14 17:24:32 -0500

    add broken function. working on it later

--- dnsthing.c
+++ dnsthing.c
@@ -4,6 +4,7 @@ void showHelp() {
 	die(
 		"Usage: dnsthing [options]\n"
 		"Example: dnsthing daemon start\n\n"
+		"  install	- install dnsthing to the current machine\n"
 		"  uninstall	- uninstall dnsthing from the current machine\n"
 		"  status	- check the current status of the daemon\n"
 		"  adlist	- manage currently used adlists\n"
@@ -12,16 +13,29 @@ void showHelp() {
 		"  daemon	- view the current stateus of the daemon\n"
 	);
 }
+void addAdlist(int argc, char *argv[]) {
+	switch(argv[1])
+	default:
+		die(
+			"Usage: dnsthing adlist [options]\n"
+			"Example: dnsthing adlist add https://foobar/\n"
+			"  add		- add an adlist to the database\n"
+			"  del		- delete an adlist from the database\n"
+			"  list		- list current adlists on the system"
+		);
+}
 
 int main(int argc, char *argv[])
 {
 	/* extra arguments/settings */
 	if (argc >= 2 && (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0))
-		die("dnsthing-%s", VER);
+		die("dnsthing-%s", VER); /* ver is actually defined in the makefile. ignore whatever error is being told here */
 	else if (argc >= 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0))
 		showHelp();
 	else if (argc >= 2 && strcmp(argv[1], "daemon") == 0)
-		viewDaemon();	
+		viewDaemon();
+	else if (argc >= 2 && strcmp(argv[1], "adlist") == 0)
+		addAdlist(argc - 1, argv + 1);
 	else if (argc != 1)
 		die("usage");
 }
